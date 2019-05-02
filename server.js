663d077f9ac9d28e756a2c667126f17d97a7a470
estherkoko <estherkolade@gmail.com>
2019-05-02 02:13:24 -0400

    added routes and sql query for fetching,updating,creating,deleting information for source or messages relating to source

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 8000
 const sqlite3 = require('sqlite3').verbose()
+const bodyParser = require("body-parser")
+
+//preprocessing to parse the body of post request
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 //start server
 app.listen(port, () => {
@@ -10,7 +15,7 @@ app.listen(port, () => {
 
 //database connection
 let db = new sqlite3.Database('./db.sqlite', (err) => {
-  if (err) {console.error(err.message);}
+  if (err) { console.error(err.message); }
   console.log('Connected to the Redox database.');
 });
 
@@ -39,7 +44,7 @@ app.get('/source', (req, res, next) => {
 app.get('/source/:id', (req, res, next) => {
   db.serialize(() => {
     let sql = `SELECT *  from source where id = ?`
-    var params = [req.params.id]
+    let params = [req.params.id]
     db.get(sql, params, (err, row) => {
       if (err) {
         res.status(400).json({ "error": err.message });
@@ -88,3 +93,79 @@ app.get('/message/:id/', (req, res, next) => {
     });
   });
 });
+
+//insert a new source with info 
+app.post('/source', (req, res, next) => {
+  let errors = []
+  if (!req.body.id) {
+    errors.push("No ID specified");
+  }
+  if (!req.body.name) {
+    errors.push("No name specified");
+  }
+  if (!req.body.environment) {
+    errors.push("No environment specified");
+  }
+  if (errors.length) {
+    res.status(400).json({ "error": errors.join(",") });
+    return;
+  }
+  let data = {
+    id: req.body.id,
+    name: req.body.name,
+    environment: req.body.environment,
+    encoding: req.body.encoding
+  }
+  let sql = `INSERT INTO source (id, name, environment, encoding) VALUES (?,?,?,?)`
+  let params = [data.id, data.name, data.environment, data.encoding]
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({ "error": err.message })
+      return;
+    }
+    res.json({
+      "message": "success",
+      "data": data,
+      "id": this.lastID
+    })
+  });
+})
+
+//update existing source information
+app.patch('/source/:id', (req, res, next) => {
+  let data = {
+    name: req.body.name,
+    environment: req.body.environment,
+    encoding: req.body.encoding
+  }
+  let params = [data.name, data.environment, data.encoding, req.params.id]
+  let sql = `UPDATE source set name = ?, environment = ?, encoding = ? WHERE id = ?`;
+  db.run(sql, params, function (err) {
+    if (err) {
+      res.status(400).json({ "error": res.message })
+      return;
+    }
+    res.json({
+      message: "success",
+      data: data,
+      changes: this.changes
+    })
+    console.log(`Row(s) updated: ${this.changes}`);
+
+  });
+
+});
+
+//delete source by id
+app.delete("/source/:id", (req, res, next) => {
+  db.run(
+      'DELETE FROM source WHERE id = ?',
+      req.params.id,
+      function (err, result) {
+          if (err){
+              res.status(400).json({"error": res.message})
+              return;
+          }
+          res.json({"message":"deleted", changes: this.changes})
+  });
+})
